514470638690a5d6c5422dc526008fd072e634db
Rozzini <Rozzinimr@gmail.com>
2020-04-17 12:13:07 +0300

    Add project files.

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result: " + FibonachiArray(N));
+        }
+
+        static int FibonachiArray(int N)
+        {
+            int result = 0;
+            int current =0;
+            int previous = 1;
+           
+
+            for(int i=0; i <N; i++)
+            {
+                if(N == 1)
+                {
+                    result = 0;
+                    break;
+                }
+                result = previous + current;
+
+
+                previous = current;
+                current = result;
+            }
+            
+
+            return result;
+        }
+    }
+}
+
---
+++ Horse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Horse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Move()
+        }
+
+        static void Move(int x1, int y1, int x2, int y2)
+        {
+            int[,] Field = new int[8, 8];
+
+            for(int i = 0; i )
+        }
+    }
+}
